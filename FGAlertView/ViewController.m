a42171f754b2d97af2e594e111a51ddffc1c28a5
DuanPengFly <304124576@qq.com>
2016-07-29 14:41:08 +0800

    DFG commit

--- FGAlertView/ViewController.m
+++ FGAlertView/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:({
+        UIImageView *avtarIV = [[UIImageView alloc] init];
+        avtarIV;
+    })];
 }
 
 - (void)didReceiveMemoryWarning {
