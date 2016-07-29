99cb71d1f2346a6f21d7579e5d672502ce6b6109
DuanPengFly <304124576@qq.com>
2016-07-29 14:36:03 +0800

    master commit

--- FGAlertView/ViewController.m
+++ FGAlertView/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:({
+        UIView *view = [[UIView alloc] init];
+        view;
+    })];
 }
 
 - (void)didReceiveMemoryWarning {
